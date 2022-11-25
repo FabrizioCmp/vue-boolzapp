aba69edabeba14d2c90524aa2a467ae283b54116
FabrizioCmp <fabriziocampari01@gmail.com>
2022-11-25 12:59:53 +0100

    Milestone 3

--- js/main.js
+++ js/main.js
@@ -5,21 +5,44 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            userList : datalist,
-            activeUser : null
+            userList: datalist,
+            activeUser: null,
+            msgInput: ""
         }
     },
     methods: {
+
+        //crea due nuovi messaggi (invio e risposta) e li aggiunge ai messaggi
+        //dell'utente attivo (di cui stiamo vedendo la chat)
+        //e poi svuota l'input
+        sendMessage() {
+            const newMessage = {
+                date: '',
+                message: this.msgInput,
+                status: 'sent'
+            }
+            const newAnswer = {
+                date: '',
+                message: 'ok',
+                status: 'received'
+            }
+            const user = this.activeUser
+            
+            this.activeUser.messages.push(newMessage);
+            //risponde dopo 1s
+            setTimeout(function(){
+                user.messages.push(newAnswer)
+            }, 1000)
+
+            this.msgInput = ""
+        },
+        
     },
-    beforeMount () {
-         this.activeUser = this.userList[0]
-      }
+    beforeMount() {
+        this.activeUser = this.userList[0]
+    }
 }).mount('#app')
 
-const userList = datalist
-const data = new Date(userList[0].messages[0].date)
-console.log(data);
-console.log(data.getHours());
-console.log(data.getMinutes());
+
 
 
